95400ebba7c1d2e107fe78742e6edb265ffe095d
jorgedaktika <jorge@daktika.com>
2023-01-23 11:38:40 +0100

    first commit

---
+++ MVCSelenium/Tests/UITests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace MVCSelenium.Tests
+{
+    public class UITests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            ChromeOptions option = new ChromeOptions();
+            option.AddArguments("--headless");
+            driver = new ChromeDriver(option);
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5153/");
+            Assert.That(driver.FindElement(By.CssSelector(".display-4")).Text, Is.EqualTo("Welcome"));
+            driver.Close();
+        }
+    }
+}
